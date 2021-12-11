6d8a6e2d210303eef84a9ef7394c38a52f99ac9d
sokmena <sokmena@gmail.com>
2021-12-11 16:37:18 +0300

    built the gallery with the initial picture

--- script/vue.js
+++ script/vue.js
@@ -1,8 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            HTMLcontent: '<a href=./index.html>Home</a> | <a href=./cv.html>Curriculum Vitae</a> | <a href=./interests.html>Interests</a> | <a href=./subscribe.html>Subscribe</a> | <a href=./contact.html>Contact</a>',
-            footer: 'Ali Sokmen - 2021'
+            HTMLcontent: '<a href=./index.html>Home</a> | <a href=./cv.html>Curriculum Vitae</a> | <a href=./interests.html>Interests</a> | <a href=./gallery.html>Gallery</a> | <a href=./subscribe.html>Subscribe</a> | <a href=./contact.html>Contact</a>',
+            footer: 'Ali Sokmen - 2021',
+            displayModal: false
         }
     },
 });
